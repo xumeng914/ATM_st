fef29c2162cd3caaedb73a820d60eee3a161b8fe
xumeng914 <xumeng320@sina.com>
2017-04-20 11:13:03 +0800

    载入tmb数据

--- loading.R
+++ loading.R
@@ -6,3 +6,13 @@ data_input_col_name <- read.table("D:\\R_project\\ATM_st\\20170412_ATM\\SDI_M_DA
                 sep = ",",quote = ",",fileEncoding = "utf8")
 colnames(data_input)<-data_input_col_name$V1
 data_input<- subset(data_input,data_input$job_seq_id=="4845142")
+
+
+## loading tmb
+data_tmb <- read.table("D:\\R_project\\ATM_st\\20170412_ATM\\SDI_F_CORE_BWFMATMB.csv",
+                         sep = ",",quote = ",",fileEncoding = "utf8")
+
+data_tmb_col_name <- read.table("D:\\R_project\\ATM_st\\20170412_ATM\\SDI_F_CORE_BWFMATMB_colname.csv",
+                                  sep = ",",quote = ",",fileEncoding = "utf8")
+colnames(data_tmb)<-data_tmb_col_name$V1
+

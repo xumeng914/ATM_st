fb76bc7d31af29d784eaec9feda3844e61208dcf
xumeng914 <xumeng320@sina.com>
2017-04-21 10:24:39 +0800

    nrom

--- norm.R
+++ norm.R
@@ -1,26 +1,9 @@
-hist(d$qx)
-
-u<-mean(d$qx)
-fc<-sd(d$qx)
-p<-1-fc/u
-n<-u/p
-
-pbinom(900,size=floor(n),prob=p)
-
-x<- c(1:floor(n))
-y <- dbinom(x,size=floor(n),prob=p)
-plot(x, y, type = "l", xlab = "binom Deviate", ylab = "Density",  yaxs = "i",xlim = c(50000,200000))
-
-
-x<- c(1:floor(n))
-y <- dnorm(x,mean=u,sd=fc)
-plot(x, y, type = "l", xlab = "norm Deviate", ylab = "Density",  yaxs = "i",xlim = c(0,200000))
 
 ###############################################################
 x <- d$qx
 png("Histogram_with_normal_curve/mygraph.png")
 h<-hist(x,
-        breaks=12,
+        breaks=30,
         col="red",
         xlab="Miles Per Gallon",
         main="Histogram with normal curve and box")
